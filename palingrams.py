e001c9aabba9ac7bce8af21ed294cd7485743d99
Denis Tikhomirov <dtikhomirov@fulgentgenetics.com>
2020-04-09 12:22:03 -0700

    Added a script that finds wordpair palingrams in a dictionary file.

---
+++ palingrams.py
@@ -0,0 +1,32 @@
+"""Find all word­pair palingrams in a dictionary file."""
+
+import load_dict
+
+def main():
+    word_list = load_dict.load('2of4brif.txt')
+    palingrams = find_palingrams(word_list)
+    palingrams_sorted = sorted(palingrams)
+    print("\nNumber of palingrams = {}\n".format(len(palingrams_sorted)))
+    for first, second in palingrams_sorted:
+        print("{} {}".format(first, second))
+
+
+def find_palingrams(word_list):
+    palingram_list = []
+    for word in word_list:
+        last_letter_index = len(word)
+        reversed_word = word[::-1]
+        if last_letter_index > 1:
+            for i in range(last_letter_index):
+                if word[i:] == reversed_word[:last_letter_index-i] and \
+                  reversed_word[last_letter_index-i:] in word_list:
+                    palingram_list.append((word,reversed_word[last_letter_index-i:]))
+                if word[:i] == reversed_word[last_letter_index-i:] and \
+                  reversed_word[:last_letter_index-i] in word_list:
+                    palingram_list.append((reversed_word[:last_letter_index-i],word))
+
+    return palingram_list
+
+
+if __name__ == "__main__":
+    main()
